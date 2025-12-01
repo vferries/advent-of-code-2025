e3abf2172d10e0715209c80a21a332db9141def5
Vincent FERRIES <vincent.ferries@gmail.com>
2025-12-01 13:58:59 +0100

    Day 1

--- src/main/kotlin/advent/Day01.kt
+++ src/main/kotlin/advent/Day01.kt
@@ -2,11 +2,58 @@ package advent
 
 object Day01 {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.fold(50 to 0) { (current, count), line ->
+            val next = turnDial(current, line)
+            next to if (next == 0) count + 1 else count
+        }.second
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var current = 50
+        var zeros = 0
+        for (line in input) {
+            val ticks = line.drop(1).toInt()
+            zeros += ticks / 100
+            when (line[0]) {
+                'L' -> {
+                    if (current == 0) {
+                        zeros--
+                    }
+                    current -= (ticks % 100)
+                    if (current == 0) {
+                        zeros++
+                    }
+                    if (current < 0) {
+                        current += 100
+                        zeros++
+                    }
+                }
+
+                'R' -> {
+                    current += (ticks % 100)
+                    if (current >= 100) {
+                        current -= 100
+                        zeros++
+                    }
+                }
+
+                else -> throw UnsupportedOperationException("Unrecognized command")
+            }
+        }
+        return zeros
+    }
+
+    fun turnDial(current: Int, instruction: String): Int {
+        val ticks = instruction.drop(1).toInt()
+        return when (instruction[0]) {
+            'L' -> {
+                val newDest = (current - ticks) % 100
+                if (newDest < 0) newDest + 100 else newDest
+            }
+
+            'R' -> (current + ticks) % 100
+            else -> throw UnsupportedOperationException("Unrecognized command")
+        }
     }
 }
 
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -1,10 +1,47 @@
-import advent.readInput
+import advent.Day01
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class Day01Test {
+    val sample = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"""
+
     @Test
-    fun `Should test something important`()  {
-        assertEquals("test content", readInput("Day01").first())
+    fun `Should correctly rotate left`() {
+        assertEquals(52, Day01.turnDial(82, "L30"))
+    }
+
+    @Test
+    fun `Should correctly rotate right`() {
+        assertEquals(14, Day01.turnDial(0, "R14"))
+    }
+
+    @Test
+    fun `Should correctly wrap around when rotating right`() {
+        assertEquals(0, Day01.turnDial(52, "R48"))
+        assertEquals(55, Day01.turnDial(95, "R60"))
+    }
+
+    @Test
+    fun `Should correctly wrap around when rotating left`() {
+        assertEquals(82, Day01.turnDial(50, "L68"))
+    }
+
+    @Test
+    fun `Should correctly compute part1 on sample`() {
+        assertEquals(3, Day01.part1(sample.lines()))
+    }
+
+    @Test
+    fun `Should correctly compute part2 on sample`() {
+        assertEquals(6, Day01.part2(sample.lines()))
     }
 }
