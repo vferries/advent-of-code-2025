825616c4c3762327cb3504d6598de9125566d3d2
Vincent FERRIES <vincent.ferries@gmail.com>
2025-12-02 12:47:58 +0100

    feat: templates

---
+++ src/main/kotlin/advent/DayTemplate.kt
@@ -0,0 +1,17 @@
+package advent
+
+object DayTemplate {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+}
+
+fun main() {
+    val input = readInput("DayTemplate")
+    DayTemplate.part1(input).println()
+    DayTemplate.part2(input).println()
+}
---
+++ src/test/kotlin/DayTemplateTest.kt
@@ -0,0 +1,12 @@
+import advent.DayTemplate
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class DayTemplateTest {
+    val sample = """"""
+
+    @Test
+    fun `Should test something part 1`() {
+        assertEquals(1, DayTemplate.part1(sample.lines()))
+    }
+}
